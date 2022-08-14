8d75e7aa67e4aa401a9291f4ef8938623cb3c8b0
named.josie <named.josie@gmail.com>
2022-08-14 00:56:30 -0400

    updated

--- src/Components/Pages/FourOFour.js
+++ src/Components/Pages/FourOFour.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 export default function FourOFour() {
     return (
       <div className="nope">
-        <h1>NOTHING TO SEE HERE </h1>
+        <h1 className="nope">NOTHING TO SEE HERE </h1>
         <img className="FOUR" src="https://i.pinimg.com/originals/52/49/2d/52492dfd578e53265da207e2903a5ce7.gif" alt="404"></img>
         <button className="new-button">
         <Link to="/">Go Home</Link>
--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -3,7 +3,7 @@ import "../styles/home.css"
 export default function Home() {
     return (
       <div className="home-page">
-        <h1 className="home-title">🎶Mesmerizing Music🎶</h1>
+        <h1 className="home-title">Mesmerizing</h1>
       <img  className="home-image" src="https://thumbs.gfycat.com/WhisperedShinyHydatidtapeworm-size_restricted.gif" alt="mag"></img>
       
       </div>
