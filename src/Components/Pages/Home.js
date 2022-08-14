c16ff54ed4787bbc2f319e13c5caafd3c620c50e
named.josie <named.josie@gmail.com>
2022-08-14 00:27:18 -0400

    updated

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -4,7 +4,7 @@ export default function Home() {
     return (
       <div className="home-page">
         <h1 className="home-title">🎶Mesmerizing Music🎶</h1>
-      <img  className="home-image" src="https://66.media.tumblr.com/tumblr_mafe51Pf3D1rfjowdo1_500.gif" alt="mag"></img>
+      <img  className="home-image" src="https://thumbs.gfycat.com/WhisperedShinyHydatidtapeworm-size_restricted.gif" alt="mag"></img>
       
       </div>
     
