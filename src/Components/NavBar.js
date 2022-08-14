178f7076b04367e3a1eb424ba52b37b76bd68e52
named.josie <named.josie@gmail.com>
2022-08-14 12:24:03 -0400

    updated

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -7,7 +7,7 @@ export default function NavBar() {
       <h1 className="header">
         <Link to="/songs">Music List</Link>
       </h1>
-      <Link to="/"> <div> <img src="https://thumbs.gfycat.com/CautiousPleasedHeifer-size_restricted.gif" alt="musix" ></img> </div></Link>
+      <Link to="/"> <div> <img className="nav-img" src="https://thumbs.gfycat.com/CautiousPleasedHeifer-size_restricted.gif" alt="musix" ></img> </div></Link>
       <button className="new-button">
         <Link to="/songs/new">New Song</Link>
       </button>
